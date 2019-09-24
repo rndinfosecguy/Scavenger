b89fb262915c4e7c895e86baba65bfa3e5ecd109
rndinfosecguy <thisisnoemail@thisdoesnotexist.com>
2019-09-24 18:25:35 +0200

    Update status.py

--- status.py
+++ status.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import sys
 import os
 import time
@@ -10,7 +8,7 @@ password_files_folder = "data/files_with_passwords"
 archive_folder = "archive"
 
 while True:
-        print """
+        print("""
 
 
         \t\t  ___|
@@ -19,20 +17,20 @@ while True:
         \t\t_____/ \___|\__,_|  \_/ \___|_|  _|\__, |\___|_|
         \t\t                                   |___/
 
-        """
+        """)
 
-        print "-----------------------------------------------------"
+        print("-----------------------------------------------------")
 
         numfiles_raw_pastes = len([f for f in os.listdir(raw_pastes_folder) if os.path.isfile(os.path.join(raw_pastes_folder, f)) and f[0] != '.'])
         numfiles_raw_pastes_pasteorg = len([f for f in os.listdir(raw_pastes_folder_pasteorg) if os.path.isfile(os.path.join(raw_pastes_folder_pasteorg, f)) and f[0] != '.'])
         numfiles_password_files = len([f for f in os.listdir(password_files_folder) if os.path.isfile(os.path.join(password_files_folder, f)) and f[0] != '.'])
         numfiles_archives = len([f for f in os.listdir(archive_folder) if os.path.isfile(os.path.join(archive_folder, f)) and f[0] != '.'])
 
-        print "|\tNumber of raw pastes (Pastebin.com): " + str(numfiles_raw_pastes)
-        print "|\tNumber of raw pastes (Paste.org): " + str(numfiles_raw_pastes_pasteorg)
-        print "|\tNumber of files with passwords: " + str(numfiles_password_files)
-        print "|\tNumber of archives: " + str(numfiles_archives)
-        print "-----------------------------------------------------"
+        print("|\tNumber of raw pastes (Pastebin.com): " + str(numfiles_raw_pastes))
+        print("|\tNumber of raw pastes (Paste.org): " + str(numfiles_raw_pastes_pasteorg))
+        print("|\tNumber of files with passwords: " + str(numfiles_password_files))
+        print("|\tNumber of archives: " + str(numfiles_archives))
+        print("-----------------------------------------------------")
 
         time.sleep(3)
         os.system("clear")
